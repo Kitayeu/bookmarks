7a668562f06e5afa1ee997cdb75e0ce0528d7715
Ivan Kitayeu <vanya.kitaev@gmail.com>
2022-01-11 11:47:44 +0300

    Added view and URL for user registration

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -23,4 +23,5 @@ urlpatterns = [
 
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
